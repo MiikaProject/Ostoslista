9240bd3d8330cb16026ba057748b083c377086e5
Miika <miika.roiha@gmail.com>
2019-08-07 13:22:47 +0300

    added possibility to add or remove items from itemlist

--- application/items/views.py
+++ application/items/views.py
@@ -31,3 +31,42 @@ def item_create():
     db.session().commit()
 
     return redirect(url_for("items.items_index"))
+
+@items.route("/items/<item_id>",methods=["GET"])
+def item_view(item_id):
+    item = Item.query.filter(Item.id==item_id).first()
+    return render_template("item.html",item=item)   
+
+@items.route("/items/<item_id>",methods=["POST"])
+def item_update(item_id):
+    #get new name and price from form
+    name = request.form.get("name")
+    price = (request.form.get("price"))
+    
+    #get item from database
+    item = Item.query.filter(Item.id==item_id).first()
+
+    #set new name and price, check for empty fields, in that case keep old values
+    if(name!=""):
+        item.name=name
+    if(price!=""):
+        price = price.replace(",",".")
+        item.price=float(price)
+
+    #commit changes to database
+    db.session.add(item)
+    db.session.commit()
+
+    return redirect(url_for("items.items_index"))
+
+@items.route("/items/remove/<item_id>",methods=["POST"])
+def item_remove(item_id):
+    #get item from database
+    item = Item.query.filter(Item.id==item_id).first()
+
+    #remove item from database
+    db.session.delete(item)
+    db.session.commit()
+
+
+    return redirect(url_for("items.items_index"))
